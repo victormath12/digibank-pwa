59315514a8485aae4dd8075fe68dfd316a57b207
Autos4U <dev.autos4u@gmail.com>
2018-03-05 00:29:46 -0300

    Application page and Camera Capture

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  alertMsg: string;
+
   title = 'Digibank';
+
+  ngOnInit () { }
+
+  onCapture (capture) {
+    this.alertMsg = 'Imagem Capturada: ' + capture.target.value;
+  }
+
+  hideAlert () {
+    this.alertMsg = undefined;
+  }
+
 }
